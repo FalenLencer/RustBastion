aa3bc348fc5da749329d4ec0302df71f492acc73
FalenLencer <clement.gaschet.45@gmail.com>
2026-05-20 00:21:43 +0200

    new

--- release/linux/rustbastion/README.txt
+++
@@ -1,9 +0,0 @@
-RUST BASTION
-============
-
-Lancement : ./launch.sh  (recommande)
-       ou : ./rustbastion
-
-Sauvegardes  : saves/rustbastion_slot*.sav
-Meta-progres : saves/rustbastion_meta.sav
-Parametres   : config/settings.bin
--- release/linux/rustbastion/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-# Audio WSLg (Windows Subsystem for Linux)
-if [ -S /mnt/wslg/runtime-dir/pulse/native ]; then
-  export PULSE_SERVER=unix:/mnt/wslg/runtime-dir/pulse/native
-fi
-exec ./rustbastion "$@"
